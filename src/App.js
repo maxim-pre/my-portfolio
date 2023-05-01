616afc59ca0f87ff2d9d9b1dbc577ee268be902c
Maxim Prestwich <maximprestwich1@gmail.com>
2023-05-01 16:17:56 +0100

    add color palette to tailwind config file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="flex items-center justify-center">my portfolio</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      backgound: "#0a192f",
+      lightBlue: "#64ffda",
+      textLight: "#ccd6f6",
+      textNormal: "#a8b2d1",
+    },
     extend: {},
   },
   plugins: [],
