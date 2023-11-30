8627dec531b8f137a73cf52828f8092459bf6ef0
Maxim Prestwich <maximprestwich1@gmail.com>
2023-11-30 14:08:20 +0000

    update about section text

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -13,29 +13,23 @@ const About = () => {
         <div className="flex flex-col sm:col-span-3 text-left text-textNormal ">
           <p className="my-2 leading-5">
             I am a programmer with strong skills in Python and JavaScript who
-            loves building web applications and has a passion for problem
-            solving. It was during my studies to become a Civil Engineer where I
-            was introduced to programming and immediately knew that this is what
-            I wanted to do. I was amazed by what you could achieve with only a
-            text editor and some time. From that point, I taught myself Python
-            alongside my degree, writing scripts to solve engineering problems
-            and used programming to research ventilation in the London
-            Underground as part of my dissertation. I love agile development, as
-            well as the feeling of progression after each completed project and
-            the sense that there’s always more to learn.
-          </p>
-          <p className="my-2 leading-5">
-            Fast-forward to today, and I've just completed a software
-            engineering bootcamp with{" "}
+            has a passion for programming. It was during my studies to become a
+            Civil Engineer where I was introduced to Python and immediately knew
+            that this is what I wanted to do. I was amazed by what you could
+            achieve with only a text editor and some time. Today I have over two
+            years of programming experience, completed a software engineering
+            bootcamp with{" "}
             <TextLink
-              label={"General Assembly "}
+              label={"General Assembly"}
               url={
                 "https://generalassemb.ly/?topic=&mkt_account_id=1056949875&mkt_campaign_id=18014985966&mkt_ad_group_id=143659598127&mkt_device_type=c&mkt_keyword=general%20assembly&mkt_matchtype=e&mkt_placement=&mkt_ad_id=616745080558&mkt_network=g&mkt_target_id=aud-1433778186441:kwd-318741616&mkt_feed_item_id=&utm_source=google&utm_medium=paid-search-bra&utm_campaign=TS:TX:BRA:LON:BR:GeneralAssembly:X:Exact&utm_content=campus-lead-lander&utm_term=general%20assembly&gclid=Cj0KCQjwmtGjBhDhARIsAEqfDEfeB_XvQZhiDoqSdi7_zBavzCcg5SBoxQ9Dsf8rASRQRjziryqbj9kaAvduEALw_wcB&gclsrc=aw.ds"
               }
             />
-            and I'm in a position to bring my web development skills to the
-            market.
+            , and am an Intern full-stack developer at Immerzo. I am constantly
+            seeking to improve my knowlege while providing value as I am both
+            results and growth orientated.
           </p>
+
           <p className="my-2 leading-5 text-textNormal">
             Here are some of the technologies and languages I am proficient
             with:
